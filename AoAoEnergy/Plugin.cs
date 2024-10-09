cf71faa9505db2d11f9a6d8c5fa6f78ea1ec0e6c
Loskh <1020612624@qq.com>
2024-10-09 23:50:54 +0800

    init

---
+++ AoAoEnergy/Plugin.cs
@@ -0,0 +1,158 @@
+﻿using Dalamud.Data;
+using Dalamud.Game;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.Hooking;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using System.Runtime.InteropServices;
+using Lumina;
+using Lumina.Excel.GeneratedSheets;
+using FFXIVClientStructs.FFXIV.Client.Game.Fate;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using Penumbra.Api.IpcSubscribers.Legacy;
+using Penumbra.Api.Enums;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace AoAoEnergy
+{
+
+    public unsafe class Plugin : IDalamudPlugin
+    {
+        public string Name => "AoAoEnergy";
+
+        [PluginService]
+        internal static IDalamudPluginInterface PluginInterface { get; set; }
+        [PluginService]
+        internal static ISigScanner Scanner { get; set; }
+        [PluginService]
+        internal static ICommandManager CommandManager { get; set; }
+        [PluginService]
+        internal static IClientState ClientState { get; set; }
+        [PluginService]
+        internal static IChatGui ChatGui { get; set; }
+        [PluginService]
+        internal static IDataManager DataManager { get; set; }
+        [PluginService]
+        internal static IGameInteropProvider GameInteropProvider { get; set; }
+        [PluginService]
+        internal static IFramework Framework { get; set; }
+        [PluginService]
+        internal static IPluginLog PluginLog { get; set; }
+        [PluginService]
+        internal static ISigScanner SigScanner { get; set; }
+        //private delegate IntPtr GetStatusDataDelegate(uint index);
+        //[Signature("E8 ?? ?? ?? ?? 0F B7 37", DetourName = nameof(GetStatusDataDetour))]
+        //private Hook<GetStatusDataDelegate> GetActionDataHook;
+        //private static object GetStatusDataDetour()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //private delegate IntPtr GetStatusHitEffectDataDelegate(uint index);
+        //[Signature("E8 ?? ?? ?? ?? 48 85 C0 74 5D 48 8B 1F ", DetourName = nameof(GetStatusHitEffectDataDetour))]
+        //private Hook<GetStatusHitEffectDataDelegate> GetStatusHitEffectDataHook;
+        //private static object GetStatusHitEffectDataDetour()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        [StructLayout(LayoutKind.Explicit, Size = 0x8)]
+        struct ActionResult
+        {
+            [FieldOffset(0x0)] public byte Type;
+            [FieldOffset(0x1)] public byte Arg0;
+            [FieldOffset(0x2)] public byte Arg1;
+            [FieldOffset(0x3)] public byte Arg2;
+            [FieldOffset(0x4)] public byte Arg3;
+            [FieldOffset(0x5)] public byte Arg4;
+            [FieldOffset(0x6)] public UInt16 Value;
+        }
+
+        private delegate void CreateResultVfxDelegate(IntPtr actionResult, Character* cast, Character* target, uint action, ActionResult* result);
+        [Signature("48 85 D2 0F 84 ?? ?? ?? ?? 53 55 57", DetourName = nameof(CreateResultVfxDetour))]
+        private Hook<CreateResultVfxDelegate> CreateResultVfxHook;
+        private void CreateResultVfxDetour(IntPtr a, Character* cast, Character* target, uint action, ActionResult* result)
+        {
+#if DEBUG
+            var status = DataManager.GetExcelSheet<Lumina.Excel.GeneratedSheets.Status>();
+            PluginLog.Debug($"{cast->NameString} {target->NameString} {result->Type} {status.GetRow(result->Value).Name}");
+#endif
+            //PluginLog.Debug($"{cast->NameString} {target->NameString} {result->Type} {result->Value} {IsShowInCarema(cast)}");
+            if (result->Type == 14 && result->Value == 49)
+            {
+                if (((nint)cast != 0 || IsShowInCarema(cast))
+                 && ((nint)target != 0 || IsShowInCarema(target)))
+                CreateVfx?.Invoke("vfx/common/eff/ev_energydrink_01x_15s.avfx", &cast->GameObject, &target->GameObject, -1, (char)0, 0, (char)0);
+            }
+            else
+            {
+                CreateResultVfxHook.Original(a, cast, target, action, result);
+            }
+
+        }
+
+        private delegate IntPtr CreateVfxDelegate(string path, GameObject* cast, GameObject* target, float speed, char a5, ushort a6, char a7);
+        private CreateVfxDelegate CreateVfx;
+
+        private delegate bool IsShowInCaremaDelegate(Character* chara);
+        private IsShowInCaremaDelegate IsShowInCarema;
+
+        private PenumbraService PenumbraService;
+
+        public Plugin()
+        {
+            GameInteropProvider.InitializeFromAttributes(this);
+            this.CreateVfx = Marshal.GetDelegateForFunctionPointer<CreateVfxDelegate>(SigScanner.ScanText("E8 ?? ?? ?? ?? 48 8B D8 48 85 C0 74 27 B2 01"));
+            this.IsShowInCarema = Marshal.GetDelegateForFunctionPointer<IsShowInCaremaDelegate>(SigScanner.ScanText("40 53 48 83 EC 20 48 8B 01 48 8B D9 FF 50 ?? 83 F8 08 75 ?? 0F B7 83"));
+            CreateResultVfxHook?.Enable();
+            InitMod();
+        }
+
+        public void InitMod()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                PenumbraService = new PenumbraService(PluginInterface);
+                Thread.Sleep(500);
+                var collection = PenumbraService._currentCollection!.Invoke(ApiCollectionType.Current);
+                var (ec, modState) = PenumbraService._getCurrentSettings!.Invoke(collection!.Value.Id, this.Name);
+                if (ec == PenumbraApiEc.ModMissing)
+                {
+                    PluginLog.Info($"Installing AoAoEnergy mod");
+                    var modPath = Path.Combine(PluginInterface.AssemblyLocation.Directory!.FullName, "AoAoEnergy.pmp");
+                    PluginLog.Debug($"{modPath}");
+                    ec = PenumbraService._installMod!.Invoke(modPath);
+                    if (ec is not PenumbraApiEc.Success)
+                        PluginLog.Error("Fail to install mod.");
+                }
+                Thread.Sleep(1000);
+                (ec, modState) = PenumbraService._getCurrentSettings!.Invoke(collection!.Value.Id, this.Name);
+                if (modState!.Value.Item1 == false)
+                {
+                    PluginLog.Info($"Enabling AoAoEnergy mod");
+                    PluginLog.Debug($"{collection!.Value.Name}");
+                    var modDir = PenumbraService._getModDir!.Invoke();
+                    ec = PenumbraService._setMod!.Invoke(collection.Value.Id, modDir, true, modName: this.Name);
+                    if (ec is not PenumbraApiEc.Success)
+                        PluginLog.Error("Fail to enable mod.");
+                };
+            });
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+            CreateResultVfxHook?.Dispose();
+            PenumbraService?.Dispose();
+        }
+    }
+}
---
+++ AoAoEnergy/ResourceLoader.cs
@@ -0,0 +1,253 @@
+﻿using Dalamud.Hooking;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.System.Resource;
+using Penumbra.String.Classes;
+using Penumbra.String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CsHandle = FFXIVClientStructs.FFXIV.Client.System.Resource.Handle;
+using FFXIVClientStructs.FFXIV.Client.System.File;
+using Dalamud.Game;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
+
+namespace AoAoEnergy
+{
+    internal unsafe class ResourceLoader
+    {
+        [StructLayout(LayoutKind.Explicit)]
+        public unsafe struct ResourceHandle
+        {
+            [StructLayout(LayoutKind.Explicit)]
+            public struct DataIndirection
+            {
+                [FieldOffset(0x00)]
+                public void** VTable;
+
+                [FieldOffset(0x10)]
+                public byte* DataPtr;
+
+                [FieldOffset(0x28)]
+                public ulong DataLength;
+            }
+
+            public readonly CiByteString FileName()
+                => CiByteString.FromSpanUnsafe(CsHandle.FileName.AsSpan(), true);
+
+            public readonly bool GamePath(out Utf8GamePath path)
+                => Utf8GamePath.FromSpan(CsHandle.FileName.AsSpan(), MetaDataComputation.All, out path);
+
+            [FieldOffset(0x00)]
+            public CsHandle.ResourceHandle CsHandle;
+
+            [FieldOffset(0x00)]
+            public void** VTable;
+
+            [FieldOffset(0x08)]
+            public ResourceCategory Category;
+
+            [FieldOffset(0x0C)]
+            public uint FileType;
+
+            [FieldOffset(0x28)]
+            public uint FileSize;
+
+            [FieldOffset(0x48)]
+            public byte* FileNameData;
+
+            [FieldOffset(0x58)]
+            public int FileNameLength;
+
+            [FieldOffset(0xA9)]
+            public byte LoadState;
+
+            [FieldOffset(0xAC)]
+            public uint RefCount;
+
+
+            // Only use these if you know what you are doing.
+            // Those are actually only sure to be accessible for DefaultResourceHandles.
+            [FieldOffset(0xB0)]
+            public DataIndirection* Data;
+
+            [FieldOffset(0xB8)]
+            public uint DataLength;
+
+            public (nint Data, int Length) GetData()
+                => Data != null
+                    ? ((nint)Data->DataPtr, (int)Data->DataLength)
+                    : (nint.Zero, 0);
+
+            public bool SetData(nint data, int length)
+            {
+                if (Data == null)
+                    return false;
+
+                Data->DataPtr = length != 0 ? (byte*)data : null;
+                Data->DataLength = (ulong)length;
+                DataLength = (uint)length;
+                return true;
+            }
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct GetResourceParameters
+        {
+            [FieldOffset(16)]
+            public uint SegmentOffset;
+
+            [FieldOffset(20)]
+            public uint SegmentLength;
+
+            public readonly bool IsPartialRead
+                => SegmentLength != 0;
+        }
+
+        public delegate void* GetResourceAsyncDelegate(IntPtr resourceManager, uint* categoryId, uint* resourceType,
+            int* resourceHash, byte* path, GetResourceParameters* resParams, bool isUnknown);
+        [Signature("E8 ?? ?? ?? 00 48 8B D8 EB ?? F0 FF 83 ?? ?? 00 00", DetourName = nameof(GetResourceAsyncDetour))]
+        private Hook<GetResourceAsyncDelegate> GetResourceAsyncHook;
+        private void* GetResourceAsyncDetour(IntPtr resourceManager, uint* categoryId, uint* resourceType,
+            int* resourceHash, byte* path, GetResourceParameters* resParams, bool isUnknown)
+        {
+            if (!Utf8GamePath.FromPointer(path, MetaDataComputation.None, out var gamePath))
+            {
+                return GetResourceAsyncHook.Original(resourceManager, categoryId, resourceType, resourceHash, path, resParams, isUnknown);
+            }
+
+            var gamePathString = gamePath.ToString();
+
+            var replacedPath = GetReplacePath(gamePathString, out var localPath) ? localPath : null;
+
+            if (replacedPath == null || replacedPath.Length >= 260)
+            {
+                var unreplaced = GetResourceAsyncHook.Original(resourceManager, categoryId, resourceType, resourceHash, path, resParams, isUnknown);
+                Plugin.PluginLog.Debug($"[GetResourceHandler] ORIGINAL: {gamePathString} -> " + new IntPtr(unreplaced).ToString("X8"));
+                return unreplaced;
+            }
+
+            var resolvedPath = new FullPath(replacedPath);
+            //PathResolved?.Invoke(*resourceType, resolvedPath);
+
+            *resourceHash = ComputeHash(resolvedPath.InternalName, resParams);
+            path = resolvedPath.InternalName.Path;
+
+            var replaced = GetResourceAsyncHook.Original(resourceManager, categoryId, resourceType, resourceHash, path, resParams, isUnknown);
+            Plugin.PluginLog.Debug($"[GetResourceHandler] REPLACED: {gamePathString} -> {replacedPath} -> " + new IntPtr(replaced).ToString("X8"));
+            return replaced;
+        
+        }
+
+        public static int ComputeHash(CiByteString path, GetResourceParameters* resParams)
+        {
+            if (resParams == null || !resParams->IsPartialRead)
+                return path.Crc32;
+
+            // When the game requests file only partially, crc32 includes that information, in format of:
+            // path/to/file.ext.hex_offset.hex_size
+            // ex) music/ex4/BGM_EX4_System_Title.scd.381adc.30000
+            return CiByteString.Join(
+                (byte)'.',
+                path,
+                CiByteString.FromString(resParams->SegmentOffset.ToString("x"), out var s1, MetaDataComputation.None) ? s1 : CiByteString.Empty,
+                CiByteString.FromString(resParams->SegmentLength.ToString("x"), out var s2, MetaDataComputation.None) ? s2 : CiByteString.Empty
+            ).Crc32;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public unsafe struct SeFileDescriptor
+        {
+            [FieldOffset(0x00)]
+            public uint FileMode;
+
+            [FieldOffset(0x30)]
+            public void* FileDescriptor;
+
+            [FieldOffset(0x50)]
+            public ResourceHandle* ResourceHandle;
+
+            [FieldOffset(0x70)]
+            public char Utf16FileName;
+        }
+
+        public delegate byte ReadSqpackDelegate(IntPtr fileHandler, SeFileDescriptor* fileDesc, int priority, bool isSync);
+        [Signature("40 56 41 56 48 83 EC ?? 0F BE 02", DetourName = nameof(ReadSqpackDetour))]
+        private Hook<ReadSqpackDelegate> ReadSqpackHook;
+        private byte ReadSqpackDetour(IntPtr fileHandler, SeFileDescriptor* fileDesc, int priority, bool isSync)
+        {
+            if (fileDesc->ResourceHandle == null) return ReadSqpackHook.Original(fileHandler, fileDesc, priority, isSync);
+
+            if (!fileDesc->ResourceHandle->GamePath(out var originalGamePath))
+            {
+                return ReadSqpackHook.Original(fileHandler, fileDesc, priority, isSync);
+            }
+
+            var originalPath = originalGamePath.ToString();
+            var isPenumbra = ProcessPenumbraPath(originalPath, out var gameFsPath);
+
+            Plugin.PluginLog.Debug($"[ReadSqpackHandler] {gameFsPath}");
+
+            var isRooted = Path.IsPathRooted(gameFsPath);
+
+            // looking for refreshed paths, could also be like |default_1|path.avfx
+            if (gameFsPath != null && !isRooted)
+            {
+                var replacementPath = GetReplacePath(gameFsPath, out var localPath) ? localPath : null;
+                if (replacementPath != null && Path.IsPathRooted(replacementPath) && replacementPath.Length < 260)
+                {
+                    gameFsPath = replacementPath;
+                    isRooted = true;
+                    isPenumbra = false;
+                }
+            }
+
+            // call the original if it's a penumbra path that doesn't need replacement as well
+            if (gameFsPath == null || gameFsPath.Length >= 260 || !isRooted || isPenumbra)
+            {
+                Plugin.PluginLog.Debug($"[ReadSqpackHandler] ORIGINAL: {originalPath}");
+                return ReadSqpackHook.Original(fileHandler, fileDesc, priority, isSync);
+            }
+
+            Plugin.PluginLog.Debug($"[ReadSqpackHandler] REPLACED: {gameFsPath}");
+
+            fileDesc->FileMode = 0;
+
+            ByteString.FromString(gameFsPath, out var gamePath);
+
+            // note: must be utf16
+            var utfPath = Encoding.Unicode.GetBytes(gameFsPath);
+            Marshal.Copy(utfPath, 0, new IntPtr(&fileDesc->Utf16FileName), utfPath.Length);
+            var fd = stackalloc byte[0x20 + utfPath.Length + 0x16];
+            Marshal.Copy(utfPath, 0, new IntPtr(fd + 0x21), utfPath.Length);
+            fileDesc->FileDescriptor = fd;
+
+            return ReadFile(fileHandler, fileDesc, priority, isSync);
+        }
+
+        private static bool ProcessPenumbraPath(string path, out string outPath)
+        {
+            outPath = path;
+            if (string.IsNullOrEmpty(path)) return false;
+            if (!path.StartsWith('|')) return false;
+
+            var split = path.Split("|");
+            if (split.Length != 3) return false;
+
+            outPath = split[2];
+            return true;
+        }
+
+        public delegate byte ReadFileDelegate(IntPtr fileHandler, SeFileDescriptor* fileDesc, int priority, bool isSync);
+        private ReadFileDelegate ReadFile = Marshal.GetDelegateForFunctionPointer<ReadFileDelegate>(Plugin.SigScanner.ScanText("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 41 54 41 55 41 56 41 57 48 81 EC ?? ?? ?? ?? 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 84 24 ?? ?? ?? ?? 48 63 42"));
+
+        public ResourceLoader()
+        {
+            Plugin.GameInteropProvider.InitializeFromAttributes(this);
+        }
+    }
+}
